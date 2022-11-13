e0525cf419ee68bed975c912ddf5372679e9ce44
Andrew <andrewelder18@gmail.com>
2022-11-12 21:22:12 -0500

    Added content for Projects and Experiences pages

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,16 +1,41 @@
-import mcmaster from './img/mcmaster.jpg'
+import mcmaster from './img/mcmaster.jpg';
+import opg from './img/opg.jpg';
+import adp from './img/adp.jpg';
 import '../css/experiances.css';
 
 function Experiences() {
   return (
-    <div className='backGroundExp backGroundSizeExp'>
-      <div>
-        <h1 className='textFont'>McMaster University</h1>
+    <div className="backGroundExp backGroundSizeExp">
+      <div className="blankSpace"></div>
+      <div className="standardBox">
+        <img className="imgBoxSize" src={mcmaster} alt="mcmaster"></img>
         <div>
-        <img src={mcmaster} alt="mcmaster"></img>
-        <p className='textFont' >This is where I went to University.</p>
+          <h1 className="textFont boxHeadline">McMaster University</h1>
+          <p className="textFont" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies efficitur euismod. Donec leo ex, sagittis eu erat non, vestibulum rutrum mi. Nullam suscipit ornare ligula, accumsan fringilla urna tempor at. Donec molestie metus turpis, ut venenatis risus laoreet eu. Ut at tortor ac dolor sollicitudin elementum ac sed sem.</p>
         </div>
       </div>
+
+
+      <div className="backColor">
+      <div className="blankSpace"></div>
+      <div className="standardBox">
+        <img className="imgBoxSize" src={opg} alt="opg"></img>
+        <div>
+          <h1 className="textFont boxHeadline">Ontario Power Generation</h1>
+          <p className="textFont" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies efficitur euismod. Donec leo ex, sagittis eu erat non, vestibulum rutrum mi. Nullam suscipit ornare ligula, accumsan fringilla urna tempor at. Donec molestie metus turpis, ut venenatis risus laoreet eu. Ut at tortor ac dolor sollicitudin elementum ac sed sem.</p>
+        </div>
+      </div>
+
+      <div className="blankSpace"></div>
+      <div className="standardBox">
+        <img className="imgBoxSize" src={adp} alt="adp"></img>
+        <div>
+          <h1 className="textFont boxHeadline">Automatic Data Processing</h1>
+          <p className="textFont" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies efficitur euismod. Donec leo ex, sagittis eu erat non, vestibulum rutrum mi. Nullam suscipit ornare ligula, accumsan fringilla urna tempor at. Donec molestie metus turpis, ut venenatis risus laoreet eu. Ut at tortor ac dolor sollicitudin elementum ac sed sem.</p>
+        </div>
+      </div>
+      </div>
+      <div className="blankSpace"></div>
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,42 @@
+import laddify from './img/laddify.png';
+import button from './img/button.jpg';
+import laser from './img/laser.jpg';
+import '../css/projects.css';
+
 
 function Projects() {
   return (
-    <div>
-      <p>
-        This is a page about projects. 
-      </p>
+    <div className="backGroundPro backGroundSizeExpPro">
+      <div className="blankSpacePro"></div>
+      <div className="standardBox">
+        <img className="imgBoxSize" src={laddify} alt="ldd"></img>
+        <div>
+          <h1 className="textFont boxHeadline">Laddify</h1>
+          <p className="textFont" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies efficitur euismod. Donec leo ex, sagittis eu erat non, vestibulum rutrum mi. Nullam suscipit ornare ligula, accumsan fringilla urna tempor at. Donec molestie metus turpis, ut venenatis risus laoreet eu. Ut at tortor ac dolor sollicitudin elementum ac sed sem.</p>
+        </div>
+      </div>
+
+
+      <div className="backColorPro">
+      <div className="blankSpacePro"></div>
+      <div className="standardBox">
+        <img className="imgBoxSize" src={button} alt="digital button"></img>
+        <div>
+          <h1 className="textFont boxHeadline">Digital button </h1>
+          <p className="textFont" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies efficitur euismod. Donec leo ex, sagittis eu erat non, vestibulum rutrum mi. Nullam suscipit ornare ligula, accumsan fringilla urna tempor at. Donec molestie metus turpis, ut venenatis risus laoreet eu. Ut at tortor ac dolor sollicitudin elementum ac sed sem.</p>
+        </div>
+      </div>
+
+      <div className="blankSpacePro"></div>
+      <div className="standardBox">
+        <img className="imgBoxSize" src={laser} alt="laser projector"></img>
+        <div>
+          <h1 className="textFont boxHeadline">Laser Projector</h1>
+          <p className="textFont" >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultricies efficitur euismod. Donec leo ex, sagittis eu erat non, vestibulum rutrum mi. Nullam suscipit ornare ligula, accumsan fringilla urna tempor at. Donec molestie metus turpis, ut venenatis risus laoreet eu. Ut at tortor ac dolor sollicitudin elementum ac sed sem.</p>
+        </div>
+      </div>
+      </div>
+      <div className="blankSpacePro"></div>
     </div>
   );
 }
