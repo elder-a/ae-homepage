94a25ba41f664cb4ed12a27563c7381be237c7fa
Andrew <andrewelder18@gmail.com>
2022-11-14 17:26:13 -0500

    Fixed Responsiveness Issues

--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -1,20 +1,41 @@
 import '../css/experiances.css';
+import { useState, useEffect } from 'react';
+
 
 function CardBox(props) {
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+      setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+
+  const isMobile = width <= 768;
+
+  
   return (
     <div>
       <div className={props.cardType}></div>
-      <div className="standardBox">
-        <img className="imgBoxSize" src={props.img} alt="not found"></img>
-        <div>
-          <h1 className="textFont boxHeadline">{props.title}</h1>
-          <h2 className=""> {props.years}</h2>
-          <p className="textFont" > {props.paragraph} </p>
-          { props.showLink === "true" &&
-          <p className="textFont" > A video about this project can be found <a href={props.videolink}>here</a>. </p>
+        <div className="standardBox">
+          { isMobile === false &&
+            <img className="imgBoxSize" src={props.img} alt="not found"></img>
           }
+          <div>
+            <h1 className="textFont boxHeadline">{props.title}</h1>
+            <h2 className=""> {props.years}</h2>
+            <p className="textFont" > {props.paragraph} </p>
+            { props.showLink === "true" &&
+            <p className="textFont" > A video about this project can be found <a href={props.videolink}>here</a>. </p>
+            }
+          </div>
         </div>
-      </div>
     </div>
   );
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,35 @@
 import "../css/base.css";
+import { useState, useEffect } from 'react';
 
 function Landing() {
+
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+      setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+
+  const isMobile = width <= 768;
+
+
+
   return (
     <div className="backGround backGroundSize ">
       <div className="blankSpaceLand"></div>
       <div className="backGroundColorLand">
-        <p className="textHiLocation typewriter">
-          Hey there,
-        </p>
+        { isMobile ===false && 
+          <p className="textHiLocation typewriter"> Hey there, </p>
+        }
+        { isMobile ===true && 
+          <p className="textHiLocation typewriter"> Hi </p>
+        }
       </div>
 
       <div className="backGroundColorLand">
